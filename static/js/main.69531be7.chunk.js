6d38a1aeab94c94ddd80424b14463c5442561ed9
robochain <87203639+robochain@users.noreply.github.com>
2024-01-27 19:23:25 +0300

    Update main.69531be7.chunk.js

--- static/js/main.69531be7.chunk.js
+++ static/js/main.69531be7.chunk.js
@@ -538,7 +538,7 @@
                     var e, t = Object(a.useContext)(w),
                         n = t.account,
                         i = function(e) {
-                            return A("0x34731aaEf796dBA871c7a053569049F7DfA49C12", z, e)
+                            return A("0x294626bee7d8104add1abe56ce427439d72f03c9", z, e)
                         }(t.signer),
                         r = Object(d.a)("(max-width:700px)"),
                         o = Object(a.useState)(""),
